03bab5c6d8acfcf3dd0a7adf493d1cf336a6407d
EvanGibsonSmith <114107230+EvanGibsonSmith@users.noreply.github.com>
2024-09-28 14:16:38 -0400

    Added solving script in terminal
    Adding solving script in terminal, but still ned to make sure each player has the proper and size and center cards are accounted for to work in an actual game

--- src/main/ClueCheater.java
+++ src/main/ClueCheater.java
@@ -1,34 +1,101 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ClueCheater {
     
-    // TODO add more rigorous tests here
-    // NOTE: delete later after tests, but this build process seems to be working.
     public static void main(String[] args) {
-        ClueGame game = new ClueGame(3);
+        // TODO rebuilding repeatedly instead of using grow is inefficient, but should work fine
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Please enter the number of players in the game");
+        int numPlayers = scnr.nextInt();
+        // TODO need to calculate proper hand size for each player, instead of asking?
+        System.out.println("Please enter the hand size of player this game");
+        int playerHandSize = scnr.nextInt();
 
-        System.out.println(Arrays.toString(game.revealedInfo()));
+        String[] rooms = {"conservatory", "dining", "ballroom", "study", "hall", "lounge", "library", "billiard"};
+        String[] people = {"scarlett", "mustard", "white", "green", "peacock", "plum"};
+        String[] weapons = {"candlestick", "knife", "lead pipe", "pistol", "rope", "wrench"};
+        
+        System.out.println("Please input player names (you will be the first player entered)");
+        String[] playerNames = new String[numPlayers];
+        for (int p=0; p<numPlayers; ++p) {
+            System.out.println("Please input player name");
+            playerNames[p] = scnr.next();
+        }
+
+        CluePlayer[] players = new CluePlayer[numPlayers]; 
+        for (int i=0; i<players.length; ++i) {
+            players[i] = new CluePlayer(playerNames[i], playerHandSize); 
+        }
+
+        // TODO could add custom card strings here to pass
+        SolverTree tree = new SolverTree(players[0], players, rooms, people, weapons); 
+
+        // initialize card information TODO need to add central card information here as well as elsewhere to make this work correctly
+        System.out.println("Please enter cards in your hand");
+        for (int i=0; i<playerHandSize; ++i) {
+            String cardStr = scnr.next();
+            players[0].getHand().addCard(new ClueCard(cardStr));
+        }
+
         ArrayList<ClueInfo> info = new ArrayList<>();
-        CluePlayer[] players =game.getPlayers();
-        SolverTree tree = new SolverTree(players[0], players);
+        while (true) {
+            System.out.println("Waiting for next piece of info...");
+            System.out.println("Please input guessing player name");
+            String guessingPlayerStr = scnr.next();
 
-        // add information that we know (if the guessing player was not THIS player, we do not know revealed card).
-        // Since we are player 0, we know the revealed card
-        //info.add(new ClueInfo(players[0], new ClueGuess("green", "dining", "lead pipe"), players[1], null, false));  // player 1 had nothing to reveal
-        //info.add(new ClueInfo(players[0], new ClueGuess("green", "dining", "lead pipe"), players[2], new ClueCard("dining"), true));  
-        // For example, below player 2 reveals something to player 1, but we do not know what it is
-        info.add(new ClueInfo(players[1], new ClueGuess("green", "hall", "pistol"), players[2], null, true));  // had something to reveal, but we don't know what (hence null card)
-        // Player two makes a guess that nobody can return
-        //info.add(new ClueInfo(players[2], new ClueGuess("scarlett", "study", "rope"), players[0], null, false)); 
-        info.add(new ClueInfo(players[2], new ClueGuess("scarlett", "study", "rope"), players[1], null, false)); 
+            System.out.println("Please input guess [person] [room] [weapon]" );
+            String guessPerson = scnr.next();
+            String guessRoom = scnr.next();
+            String guessWeapon = scnr.next();
 
-        tree.build(info.toArray(new ClueInfo[0]));
-        System.out.println("BUILD OUTCOME");
-        for (int layerIdx=0; layerIdx<10; ++layerIdx) {
-            System.out.println(Arrays.toString(tree.getTree().getNodesLayer(layerIdx)));
-            System.out.println("\n\n\n");
+            System.out.println("Please input true or false for if a card was shown");
+            boolean hasCard = scnr.nextBoolean();
+
+            if (hasCard) {
+                System.out.println("Please input player that revealed card");
+            }
+            else {
+                System.out.println("Please input player that did not reveal information");
+            }
+            String revealingPlayerStr = scnr.next();
+
+            // if this player was shown a card, ask which card it was. Otherwise it will be null since they didn't know
+            String card = null;
+            if (guessingPlayerStr.equals(players[0].getName()) && hasCard) { 
+                System.out.println("Please input which card was revealed");
+                card = scnr.next();
+            }
+
+            // add information to tree
+            // set up guess
+            ClueGuess guess = new ClueGuess(guessPerson, guessRoom, guessWeapon);
+            // get correct player objects for guessingPlayer and revealingPlayer
+            CluePlayer guessingPlayer = null; // will be set if a correct player string was suppliied
+            CluePlayer revealingPlayer = null;
+            for (CluePlayer player: players) {
+                if (player.getName().equals(guessingPlayerStr)) {
+                    guessingPlayer = player;
+                }
+                if (player.getName().equals(revealingPlayerStr)) {
+                    revealingPlayer = player;
+                }
+            }
+            info.add(new ClueInfo(guessingPlayer, guess, revealingPlayer, new ClueCard(card)));
+
+            // check if answer can be deduced
+            tree.build(info.toArray(new ClueInfo[0])); // rebuild tree (quite inefficient but works)
+            ClueCard[] answer = tree.getAnswer();
+            if (answer==null) {
+                System.out.println("No answer found yet!");
+            }
+            else {
+                System.out.println("Answer: " + Arrays.toString(answer));
+            }
+            System.out.println(tree.getAnswer());
         }
     }
 }
--- src/main/ClueGuess.java
+++ src/main/ClueGuess.java
@@ -6,23 +6,23 @@ import java.util.HashSet;
 // TODO document
 // TODO for the sake of the solver, ClueCard objects aren't needed. Could make a trimmed down version later
 public class ClueGuess {
-    ClueCard player;
+    ClueCard person;
     ClueCard room;
     ClueCard weapon;
     Set<ClueCard> cardSet = new HashSet<>();
 
-    public ClueGuess(String player, String room, String weapon) {
-        this.player = new ClueCard(player);
+    public ClueGuess(String person, String room, String weapon) {
+        this.person = new ClueCard(person);
         this.room = new ClueCard(room);
         this.weapon = new ClueCard(weapon);
         populateCardSet();
     }
 
-    public ClueGuess(ClueCard player, ClueCard room, ClueCard weapon) {
-        if (player.getType()!="player" | room.getType()!="roon" | weapon.getType()!="weapon") {
+    public ClueGuess(ClueCard person, ClueCard room, ClueCard weapon) {
+        if (person.getType()!="player" | room.getType()!="roon" | weapon.getType()!="weapon") {
             throw new RuntimeException("Types of inputted cards much match player, room, weapon.");
         }
-        this.player = player;
+        this.person = person;
         this.room = room;
         this.weapon = weapon;
         populateCardSet();
@@ -30,7 +30,7 @@ public class ClueGuess {
 
     @Override 
     public String toString() {
-        return player.toString() + ", " + room.toString() + ", " + weapon.toString();
+        return person.toString() + ", " + room.toString() + ", " + weapon.toString();
     }
 
     @Override 
@@ -47,7 +47,7 @@ public class ClueGuess {
     }
 
     private void populateCardSet() {
-        cardSet.add(this.player);
+        cardSet.add(this.person);
         cardSet.add(this.room);
         cardSet.add(this.weapon);
     }
